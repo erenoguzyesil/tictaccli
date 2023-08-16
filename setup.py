28c5d8d29205c36a78796e7e224f46af9dd3c6b2
erenoguzyesil <erenoguzyesil29@gmail.com>
2023-08-17 00:32:48 +0300

    Made changes to setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,14 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name='tictactoecli',
+    name='tictaccli',
     version='1.0.0',
     description='A simple Tic-Tac-Toe game in command-line.',
     long_description=long_description,
     long_description_content_type='text/markdown',
+    keywords='cli game tictactoe commandline',
     url='https://github.com/erenoguzyesil/tictactoecli',
+    license='MIT',
     packages=find_packages(),
     entry_points={
         'console_scripts': ['tictactoe=tictactoecli:play']
